c620337dbcdf11d9a62e7ddd68f5f36d51870899
Robert Wiebkin <Rwiebkin@peterbrett.com>
2020-03-11 13:11:00 +0000

    added XO's report

--- Jaeger/Entities/CombatMenu.cs
+++ Jaeger/Entities/CombatMenu.cs
@@ -72,7 +72,7 @@ namespace MeerJager.Entities
                 Console.WriteLine("{0}. {1}", option.Id, option.Display);
             }
             var key = Console.ReadKey().KeyChar;
-            
+            Console.Clear();
             if (char.IsDigit(key) && menuOptions.Any(x => x.Id.ToString() == key.ToString()))
             {
                 var selected = menuOptions.Where(x => x.Id.ToString() == key.ToString()).FirstOrDefault();
@@ -93,7 +93,7 @@ namespace MeerJager.Entities
                         player.LowerDepth();
                         break;
                     case 5:
-                        
+                        SituationReport(enemy);
                         break;
                     default:
                         break;
@@ -102,12 +102,50 @@ namespace MeerJager.Entities
         }
 
 
+        public static void SituationReport(Enemy enemy)
+        {
+            Console.Clear();
+            Console.WriteLine("XO's report:");
+            if (enemy.playerCanSee)
+            {
+                Console.WriteLine("Captain, we have one enemy at {0}km ahead", enemy.DistanceToPlayer/100);
+                if (enemy.isEngaged)
+                {
+                    Console.WriteLine("They are aware of our presence and are moving to engage");
+                }
+            }
+            Console.WriteLine("We have {0} torpedos in the stores with {1} loaded into tubes", player.Torpedos, 0);
+            Console.WriteLine("Currently we are at {0} depth", player.Depth.DepthName);
+            if (0 < player.Health && player.Health >= 20)
+            {
+                Console.WriteLine("Our hull is critically compormised");
+            }
+            else if (20 < player.Health && player.Health >= 60)
+            {
+                Console.WriteLine("Our hull is heavily damaged");
+            }
+            else if (20 < player.Health && player.Health >= 60)
+            {
+                Console.WriteLine("Our hull is damaged");
+            }
+            else if (60 < player.Health && player.Health >= 99)
+            {
+                Console.WriteLine("Our hull is slightly damaged");
+            }
+            else
+            {
+                Console.WriteLine("Our hull untouched");
+            }
+
+            Console.WriteLine("What are your orders?");
+            GetPlayerChoice(enemy);
+        }
 
         public static void CombatRound(Enemy enemy)
         {
 
             Console.WriteLine("Round Start");
-            
+            Console.WriteLine();
             if (!enemy.isEngaged)
             {
                 EnemySearching(enemy); 
@@ -141,9 +179,6 @@ namespace MeerJager.Entities
                 double hundredMetersToPlayer = enemy.DistanceToPlayer / 100;
                 double finalDetectionChance = baseDetectionChancePerHundredMeters * Math.Floor(hundredMetersToPlayer);
                 int roll = Dice.RollPercentage();
-
-                Console.WriteLine("DetectionChance: " + finalDetectionChance);
-                Console.WriteLine("Roll: " + roll);
                 if (finalDetectionChance > roll)
                 {
                     enemy.Engage();
